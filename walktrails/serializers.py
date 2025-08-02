4c61856e1db8be44cc386b5915fc2b57a272c13c
dangyulee <dg200101@gmail.com>
2025-08-03 03:09:09 +0900

    walktrail 작업(완)

--- walktrails/serializers.py
+++ walktrails/serializers.py
@@ -6,18 +6,35 @@ from .models import WalkTrail, Route
 class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
-        fields = ['id', 'trail', 'name', 'latitude', 'longitude']
-        read_only_fields = ['id']
+        fields = ['name', 'latitude', 'longitude']
 
+class SimpleRouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ['name']
 class WalkTrailSerializer(serializers.ModelSerializer):
+    # 산책로 정보(리스트 나열용)
     class Meta:
         model = WalkTrail
-        fields = ['id', 'name', 'duration', 'distance_km', 'description', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ['name', 'duration', 'distance_km']
 
-class WalkTrailDetailSerializer(serializers.ModelSerializer):
-    routes = RouteSerializer(many=True, read_only=True)
+class WalkTrailInfoSerializer(serializers.ModelSerializer):
+    # 산책로 선택 페이지(정보)
+    routes = serializers.SerializerMethodField()
+
     class Meta:
         model = WalkTrail
-        fields = ['id', 'name', 'duration', 'distance_km', 'description', 'created_at', 'routes']
-        read_only_fields = ['id', 'created_at']
+        fields = ['name', 'duration', 'distance_km', 'description', 'routes']
+
+    def get_routes(self, obj):
+        queryset = obj.routes.all().order_by('order')
+        filtered = queryset[0::5]  # 0부터 5개씩 띄엄띄엄
+        return SimpleRouteSerializer(filtered, many=True).data
+    
+class WalkTrailRouteSerializer(serializers.ModelSerializer):
+    # 산책로 루트 출력
+    routes = RouteSerializer(many = True)
+    
+    class Meta:
+        model = WalkTrail
+        fields = ['name', 'routes']
--- walktrails/views.py
+++ walktrails/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.db.models.query import QuerySet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
+
+from . import serializers
+from . import models
 
 # Create your views here.
+class WalkTrail_list(generics.ListAPIView):
+    # walktrail 리스트 조회
+    queryset = models.WalkTrail.objects.all()
+    serializer_class = serializers.WalkTrailSerializer
+    pagination_class = None
+
+class WalkTrail_info(generics.RetrieveAPIView):
+    # walktrail 정보 조회
+    queryset = models.WalkTrail.objects.all()
+    serializer_class = serializers.WalkTrailInfoSerializer
+    lookup_url_kwarg = 'name'    
+
+class GetRoute(generics.RetrieveAPIView):
+    # 해당 산책로 좌표정보 받아오기
+    queryset = models.WalkTrail.objects.all()
+    serializer_class = serializers.WalkTrailRouteSerializer
+    lookup_field = 'name'
+    lookup_url_kwarg = 'name'
+
+class GetRouteAll(generics.ListAPIView):
+    # 전체 산책로 좌표정보 받아오기 / 관리자용
+    queryset = models.WalkTrail.objects.all()
+    serializer_class = serializers.WalkTrailRouteSerializer
+    pagination_class = None
