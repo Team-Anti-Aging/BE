785ea488f9068e099787e0c5c5ea21d1615cf755
Trashbin4943 <anthonyhwan415@gmail.com>
2025-08-18 15:51:10 +0900

    관리자 페이지 최근 신고내역 파트 리팩토링

--- admin_func/views.py
+++ admin_func/views.py
@@ -9,6 +9,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from feedback.models import Feedback, WalkTrail
 from django.db.models import Count, Q
+from django.utils.timezone import now
+from feedback.serializers import OnlyFeedbackSerializer
 
 # 각 산책로별 피드백 현황
 class FeedbackinProgress(APIView):
@@ -29,9 +31,41 @@ class CurrentFeedbackList(generics.ListAPIView):
         walktrail_name = self.kwargs.get('walktrail_name')
         qs = WalkTrail.objects.filter(feedback__status='in_progress', name=walktrail_name)
         # 피드백의 생성시간 기준 내림차순 정렬, 상위 5개
-        return qs.order_by('-feedback__created_at')[:5]
+        return qs.order_by('-feedback__created_at')
 
 
+# 금일 신고 내역 (산책로 별 / (불편, 제안) 별)
+# ==============================================================================================
+class TodayFeedbackView(generics.ListAPIView):
+    serializer_class=CurrentFeedbackSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        today = now().date()
+        qs = Feedback.objects.filter(created_at__date=today).values(
+            'walktrail__name', 'type'
+        ).annotate(count=Count('id')).order_by('walktrail__name', 'type')
+        return Response(list(qs))
+
+# 특정 산책로의 최근 피드백
+class RecentFeedbackView(generics.ListAPIView):
+    serializer_class = OnlyFeedbackSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        walktrail_name = self.kwargs.get('walktrail_name')
+        pk = self.kwargs.get('pk')  # 선택적으로 들어올 수 있음
+        qs = Feedback.objects.filter(walktrail__name=walktrail_name, status='in_progress')
+
+        if pk is not None:
+            qs = qs.filter(pk=pk)  # 특정 feedback 하나만
+        else:
+            qs = qs.order_by('-created_at')  # 리스트일 경우 최신순
+
+        return qs
+# ============================================================================================================
+
+# 피드백에 대한 응답 생성 뷰
 class ResponseCreateView(generics.CreateAPIView):
     queryset = ResponseModel.objects.all()
     serializer_class = ResponseCreateSerializer
