7481395d2ca6e3c08a693e0a239b562f4cf81e6a
Dangyu <100976893+dangyulee@users.noreply.github.com>
2025-08-21 03:50:05 +0900

    Update settings.py

--- antiaging/settings.py
+++ antiaging/settings.py
@@ -221,7 +221,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_EMAIL_REQUIRED = False
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5), # 액세스 토큰의 유효기간 # 재발급 테스트용
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=4), # 액세스 토큰의 유효기간
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1), # 리프레시 토큰의 유효기간
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
